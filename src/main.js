c50bf609716d6a79af6aa3e8c21f093df2e1c5a2
Hoang Hy <hoanghy01122003@gmail.com>
2022-03-02 20:10:46 +0700

    Add new endpoint
     Add /about endpoint 
    It will send 'This is about page' to the user when they enter /about

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
    return res.send('This is the homepage')
 })
 
+app.get('/about', (_, res) => {
+   return res.send('This is about page')
+})
+
 app.listen(port, () => {
    console.log(`Listening on port ${port}`)
 })
